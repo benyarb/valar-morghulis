88b5748673dd19c32f2162b5852176ba36134c0d
Ben Yarbrough <benyarb@gmail.com>
2023-06-10 18:34:16 -0400

    Fix text clipping in heading on hover

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <header className="mt-12 px-12 text-center">
-          <div className="brand max-w-lg mx-auto flex justify-center items-end gap-2 pb-8 pt-48">
-            <h1 className="text-4xl leading-9 text-left">Valar Morghulis</h1>
-            <p className="text-sm">on</p>
+          <div className="brand max-w-lg mx-auto flex justify-center gap-2 pb-8 pt-48">
+            <h1 className="text-4xl text-left">Valar Morghulis</h1>
+            <p className="text-sm leading-6 self-end">on</p>
 
             <Image
               className="relative pb-1 dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
